8d6942fff59610704c38e86f2d09d18792e7be5f
kanyanat <fakchim_k@silpakorn.edu>
2026-09-26 12:11:52 +0700

    อัพpokemon api

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//pokemon api//
+
 const express = require("express");
 const fs = require("fs");
 
@@ -9,3 +11,12 @@ app.use(express.static("public"));
 
 const API_URL = "https://pokeapi.co/api/v2/pokemon?limit=1025";
 let pokemons = [];
+
+//โหลดเอา pokemon จาก pokeapi//
+
+async function loadPokemons() {
+    console.log("กำลังโหลดข้อมูล Pokémon อยู่จ้า . . .");
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    console.log(data);
+}
