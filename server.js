faa87cff1720398603d255b2de90535354cbf8a9
kanyanat <fakchim_k@silpakorn.edu>
2026-09-26 12:34:36 +0700

    อัพpokemon api ครั้ง2

--- server.js
+++ server.js
@@ -15,8 +15,27 @@ let pokemons = [];
 //โหลดเอา pokemon จาก pokeapi//
 
 async function loadPokemons() {
+
     console.log("กำลังโหลดข้อมูล Pokémon อยู่จ้า . . .");
     const response = await fetch(API_URL);
     const data = await response.json();
     console.log(data);
+    const results = await Promise.all(
+
+    data.results.map(async (pokemon) => {
+        const response = await fetch(pokemon.url);
+        const detail = await response.json();
+    //เอาค่าต่าง ๆ ของ pokemon//
+        const pokemonData = {
+            id: detail.id,
+            name: detail.name,
+            image: detail.sprites.front_default,
+            hp: detail.stats[0].base_stat,
+            attack: detail.stats[1].base_stat,
+            defense: detail.stats[2].base_stat
+        };
+        return pokemonData;
+    })
+
+);
 }
