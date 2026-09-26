3c3946f2a5c8900e98cd6df4ae637e0e5bc7bbb6
kanyanat <fakchim_k@silpakorn.edu>
2026-09-26 11:52:25 +0700

    อัพapi

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const API_URL =
-    "https://pokeapi.co/api/v2/pokemon?limit=1025";
+const express = require("express");
+const fs = require("fs");
 
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+const API_URL = "https://pokeapi.co/api/v2/pokemon?limit=1025";
 let pokemons = [];
